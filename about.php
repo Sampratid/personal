c95041b510750b993fed4ebaef445cdd975b631d
samprati <samprati@pari.com.my>
2020-01-10 10:29:57 +0800

    Initial commit

---
+++ about.php
@@ -0,0 +1,440 @@
+<!DOCTYPE html>
+<html lang="en">
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="abstract" content="Software Company Malaysia" />
+    <meta name="copyright" content="PARI TECH : Software Company Malaysia" />
+	<title>Software Company Malaysia | Web Application Development | Software Application Development</title>
+	<meta name="description" content="Software Company Malaysia : Get your business and departments automated and operationally optimised via expert system development via web applications and software solutions" />
+	<meta name="keywords"  content="software,company,malaysia,web,application,development,application" />
+    <meta name="robots" content="follow, index" />
+	<meta name="Resource-type" content="Document" />
+    <meta name="format-detection" content="telephone=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+    
+    <link rel="canonical" href="http://pari.com.my" />
+    
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Software Company Malaysia" />
+    <meta property="og:description" content="Software Company Malaysia : Get your business and departments automated and operationally optimised via expert system development via web applications and software solutions" />
+    <meta property="og:url" content="http://pari.com.my" />
+    <meta property="og:site_name" content="Software Company Malaysia" />
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" /><!-- Styles -->
+	<link href="js/owl-carousel/owl.carousel.css" rel="stylesheet" />
+	<link href="js/owl-carousel/owl.theme.css" rel="stylesheet" />
+	<link href="js/owl-carousel/owl.transitions.css" rel="stylesheet" />
+	<link href="js/rs-plugin/css/settings.css" rel="stylesheet" />
+	<link href="js/flexslider/flexslider.css" rel="stylesheet" />
+	<link href="js/isotope/isotope.css" rel="stylesheet" />
+	<link href="css/jquery-ui.css" rel="stylesheet" />
+	<link href="js/magnific-popup/magnific-popup.css" rel="stylesheet" />
+	<link href="css/style.css" rel="stylesheet" />
+	<link href="css/responsive.css" rel="stylesheet" /><!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Karla:400,700" rel="stylesheet">
+	<link href="css/icomoon/style.css" rel="stylesheet" type="text/css" />
+	<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+	<link href="images/favicon.ico" rel="shortcut icon" /><!-- SKIN -->
+	<link href="css/color-scheme/red.css" rel="stylesheet" type="text/css" />
+<SCRIPT language=JavaScript>
+
+
+
+var message = "You cannot view this: You are not doing it right";
+
+function rtclickcheck(keyp){ if (navigator.appName == "Netscape" && keyp.which == 3){ alert(message); return false; }
+
+if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) { alert(message); return false; } }
+
+document.onmousedown = rtclickcheck;
+
+</SCRIPT>
+<style>
+.clients{
+    width:100%;
+}
+
+@media only screen and (min-width: 365px) and (max-width: 500px) {
+.client_mobile{
+    margin-left:0% !important;
+}
+
+}
+</style>
+</head>
+<body id="about-page">
+<div id="main-wrapper"><!-- offcanvas-menu end -->
+<div class="offcanvas-pusher">
+<div class="content-wrapper">
+<div class="outer-wrapper"><!-- HEADER -->
+<header id="header-main">
+<div class="container">
+<div class="navbar yamm navbar-default">
+<div class="navbar-header"><button class="navbar-toggle" data-target="#navbar-collapse-1" data-toggle="collapse" type="button"></button><a class="navbar-brand" href="../index.php"><img alt="" src="images/logo-red.png" style="width: 40px;margin-top: -12px;" /></a></div>
+
+<div class="navbar-collapse collapse navbar-right" id="navbar-collapse-1">
+<ul class="nav navbar-nav snip1135">
+	<li><a href="../index.php">Home</a></li>
+	<li class="current"><a href="about.html" style="color:#c70039;font-weight: bold; font-size: 15px;">About Us</a></li>
+	<li><a href="services.html">Services</a></li>
+	<li><a href="talentstar.php">Product</a></li>
+	<li><a href="news.html">News</a></li>
+	<li><a class="dropdown-toggle" data-toggle="dropdown" href="#" style="color:#c70039;font-weight: bold; font-size: 15px;">More..</a>
+	<ul class="dropdown-menu">
+		<li><a href="careers.html">Careers</a></li>
+		<li><a href="contact.html">Contact Us</a></li>
+	</ul>
+	</li>
+</ul>
+</div>
+</div>
+</div>
+</header>
+<!-- PAGE HEADER -->
+
+<div class="page_header">
+<div class="page_header_parallax">
+<div class="container">
+<div class="row">
+<div class="col-md-12">
+<h3><span>About Us</span>We believe in being futuristic.</h3>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="sky-tabs sky-tabs-amount-2 sky-tabs-pos-top-justify sky-tabs-anim-flip sky-tabs-response-to-icons"><input checked="checked" class="sky-tab-content-1" id="sky-tab1" name="sky-tabs" type="radio" /> <label for="sky-tab1"><span><span>Company</span></span></label> <input class="sky-tab-content-2" id="sky-tab2" name="sky-tabs" type="radio" /> <label for="sky-tab2"><span><span>Values</span></span></label> 
+
+<!-- 		<input class="sky-tab-content-3" id="sky-tab3" name="sky-tabs" type="radio" /> 
+<label for="sky-tab3"><span><span>Team</span></span></label> -->
+ <!-- <input type="radio" name="sky-tabs" id="sky-tab4" class="sky-tab-content-4">
+                  <label for="sky-tab4"><span><span><i class="fa fa-cogs"></i>Lab</span></span></label> -->
+
+<ul>
+	<li class="sky-tab-content-1">
+	<div class="typography">&nbsp;
+	<div class="col-md-8 about-left" style="margin-left:15%">
+
+	<h1 style="text-align:center;">About Us</h1>
+	<h5 style="font-size: 19px;line-height: 34px; text-align:center;">PARI Technologies started from a passion of creating mobile and web applications to re-invent traditional processes through technology.</h5>
+	<h5 style="font-size: 19px;line-height: 34px; text-align:center;">Our purpose is to create seamless digital experiences & easy solutions which empowers the user and changes the feel of the interaction.</h5>
+	
+	<h1 style="text-align:center;">Why we do what we do ?</h1>
+
+	<h5 style="font-size: 19px;line-height: 34px; text-align:center;">We want to push boundaries. We want to play a role in shaping the future and development of digital processes. We believe in digital empowerment. We create mobile & web applications which are visually exquisite, easy to use & user friendly. We aim to design to revolutionize user experience.</h5>
+
+	</div>
+	<br />
+	<!-- <a href="#" class="sky-tab-content-2"> -->
+	<div class="col-md-8 col-md-offset-2 text-center client_mobile" style="margin-left:15%"><br />
+	<br />
+	&nbsp;
+	<h1 style="text-align:center;">Our Clients</h1>
+	&nbsp;
+
+
+    <!-- <img alt="..." class="img-res-1 col-md-2 col-sm-2 col-xs-2 logo " src="images/Logo-whiteark.png" style="width:10%" /> -->
+    
+            <div class="row text-center">
+
+            <div class="col-md-3 col-md-6 mb-4">
+            <div class="card h-100">
+                <img class="card-img-top clients" src="images/clients/whiteark.png" alt="">
+                
+            </div>
+            </div>
+            
+            <div class="col-md-3 col-md-6 mb-4">
+            <div class="card h-100">
+                <img class="card-img-top clients" src="images/clients/olive.png" alt="">
+                
+            </div>
+            </div>
+
+            <div class="col-md-3 col-md-6 mb-4">
+            <div class="card h-100">
+                <img class="card-img-top clients" src="images/clients/vin.png" alt="">
+                
+            </div>
+            </div>
+
+            <div class="col-md-3 col-md-6 mb-4">
+            <div class="card h-100">
+                <img class="card-img-top clients" src="images/clients/kdj.png" alt="">
+                
+            </div>
+            </div>
+
+            <div class="col-md-3 col-md-6 mb-4">
+            <div class="card h-100">
+                <img class="card-img-top clients" src="images/clients/rmj.png" alt="">
+                
+            </div>
+            </div>
+
+            <div class="col-md-3 col-md-6 mb-4">
+            <div class="card h-100">
+                <img class="card-img-top clients" src="images/clients/asap.png" alt="">
+                
+            </div>
+            </div>
+
+            <div class="col-md-3 col-md-6 mb-4">
+            <div class="card h-100">
+                <img class="card-img-top clients" src="images/clients/short.png" alt="">
+                
+            </div>
+            </div>
+
+
+
+            </div>
+
+
+    <!-- <img alt="..." class="img-res-2 col-md-3  logo img-responsive" src="images/olive.png" style="width:17%;left: 3%;" /> 
+	<img alt="..." class="img-res-4 col-md-3 logo img-responsive" src="images/logo1-demo.png" style="width:16%;left: 5%;" />
+	<img alt="..." class="img-res-4 col-md-3 logo img-responsive" src="images/kdj.png" style="width:16%;left: 5%;" />
+	<img alt="..." class="img-res-4 col-md-3 logo img-responsive" src="images/RMJ_logo.png" style="width:16%;left: 5%;" />
+    <img alt="..." class="img-res-4 col-md-3 logo img-responsive" src="images/asap.png" style="width:16%;left: 5%;" />
+     -->
+
+	</div>
+	<!-- </a> --></div>
+	</li>
+	<li class="sky-tab-content-2">
+	<div class="typography">
+	<h1>Shared Values</h1>
+	&nbsp;
+
+	<div class="row">
+	<div style="background: url('images/2.jpg') no-repeat center center; background-size: cover; height: 50vh;">
+	<h1 style="text-align: center;padding-top: 5%;color: white;" class="tab-content">- We Care -</h1>
+
+	<p style="text-align: center;color: white;font-size: 22px;
+                                 line-height: 40px;width: 80%;
+    margin-left: 10%;">We nurture relationships. When we are approached with an idea,a need or a vision our mission is to make the idea a success. We share our views and show you different alternatives &amp; possibilities on how to transform the idea into a successful reality.</p>
+	</div>
+
+	<div style="background: url('images/3.jpg') no-repeat center center; background-size: cover; height: 56vh;">
+	<h1 style="text-align: center;padding-top: 5%;color: white;"class="tab-content">- We Love what we do -</h1>
+
+	<p style="text-align: center;color: white;font-size: 22px;
+                                 line-height: 40px;width: 80%;
+    margin-left: 10%;">We are not in just for the buck. Our passion lies in providing different experiences to different users which are driven by a need and are solved with a simplified way. Our commitment towards our work is what drives the company culture. Every project is our top priority.</p>
+	</div>
+
+	<div style="background: url('images/4.jpg') no-repeat center center; background-size: cover; height: 50vh;">
+	<h1 style="text-align: center;padding-top: 5%;color: white;" class="tab-content">- We create enriching digital experiences -</h1>
+
+	<p style="text-align: center;color: white;font-size: 22px;
+                                 line-height: 40px;width: 80%;
+    margin-left: 10%;">We believe experience is an emotion. Our aim to give users an experience which gives them the &#39;wow&#39; feeling and are bound to share their experience with people. Our team is continuously looking into incorporating innovative ideas, concepts and cutting edge approaches in all our products to enhance the user experience.</p>
+	</div>
+
+	<div style="background: url('images/5.jpg') no-repeat center center; background-size: cover; height: 50vh;">
+	<h1 style="text-align: center;padding-top: 5%;color: white;" class="tab-content">- We Think Innovatively -</h1>
+
+	<p style="text-align: center;color: white;font-size: 22px;
+                                 line-height: 40px;width: 80%;
+    margin-left: 10%;">We like to stay ahead of the game. We update ourselves regularly with the latest developments and useful insights as it makes us think outside the box and come back in to implement it. Our strength lies providing positive criticism, discussing, brainstorming and challenging feasibility of ideas which enables us to transform ideas into reality.</p>
+	</div>
+
+	<div style="background: url('images/6.jpg') no-repeat center center; background-size: cover; height: 50vh;">
+	<h1 style="text-align: center;padding-top: 5%;color: white;" class="tab-content">- We Celebrate -</h1>
+
+	<p style="text-align: center;color: white;font-size: 22px;
+                                 line-height: 40px;width: 80%;
+    margin-left: 10%;">We win or we learn. Our day to day discoveries and our attitude of rejoicing learning new concepts helps us to rejuvenate ourselves and continue on the journey we are on.</p>
+	</div>
+	</div>
+	</div>
+	</li>
+	<?php
+
+	$s = '
+		<li class="sky-tab-content-3" style="width: 80%;margin-left: 12%;">
+		<div class="typography">
+		<h1>Our Team</h1>
+		<br />
+		<!-- TEAM MEMBERS SECTION --><!-- TEAM -->
+		<div class="row">
+		<div class="col-md-12">
+		<div class="col-lg-12">
+		<div class="row pt-md">
+		<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
+		<div class="img-box"><img class="img-responsive" src="images/team/babar-3.png" style="filter: grayscale(100%);" /></div>
+	
+		<h1>Babar Khan Rafat</h1>
+	
+		<h2>Co-Founder/ CEO</h2>
+		</div>
+	
+		<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
+		<div class="img-box"><img class="img-responsive" src="images/team/me.png" style="filter: grayscale(100%);" /></div>
+	
+		<h1>Samprati Dash</h1>
+	
+		<h2>Co-Founder/ CTO</h2>
+		</div>
+	
+		<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
+		<div class="img-box"><img class="img-responsive" src="images/team/annuar.png" style="filter: grayscale(100%);" /></div>
+	
+		<h1>Khairul Annuar</h1>
+	
+		<h2>COO</h2>
+		</div>
+	
+		<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
+		<div class="img-box"><img class="img-responsive" src="images/team/ravi.png" style="filter: grayscale(100%);" /></div>
+	
+		<h1>Ravi Patel</h1>
+	
+		<h2>Head of Business Development</h2>
+		</div>
+		</div>
+	
+		<div class="row">
+		<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
+		<div class="img-box"><img class="img-responsive" src="images/team/naga.png" style="filter: grayscale(100%);" /></div>
+	
+		<h1>Nagaraja Muthaya</h1>
+	
+		<h2>Customer Support</h2>
+		</div>
+	
+		<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
+		<div class="img-box"><img class="img-responsive" src="images/team/anis.png" style="filter: grayscale(100%);" /></div>
+	
+		<h1>Anis Derbel</h1>
+	
+		<h2>Lead Developer</h2>
+		</div>
+	
+		<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
+		<div class="img-box"><img class="img-responsive" src="images/team/sheila.png" style="filter: grayscale(100%);" /></div>
+	
+		<h1>Sheila Wati</h1>
+	
+		<h2>Account Manager</h2>
+		</div>
+		<!-- <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
+		<div class="img-box"><img class="img-responsive" src="images/team/sipu-1.png" style="filter: grayscale(100%);" /></div>
+	
+		<h1>Samsruti Dash</h1>
+	
+		<h2>Mobile (Android) Developer</h2>
+		</div> -->
+	
+		<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
+		<div class="img-box"><img class="img-responsive" src="images/team/peggy.png" style="filter: grayscale(100%);" /></div>
+	
+		<h1>Peggy Tang</h1>
+	
+		<h2>Designer</h2>
+		</div>
+		</div>
+	
+		<div class="row">
+		<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile"></div>
+	
+		<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
+		<div class="img-box"><img class="img-responsive" src="images/team/loo.png" style="filter: grayscale(100%);" /></div>
+	
+		<h1>Yen Loo</h1>
+	
+		<h2>Web Developer</h2>
+		</div>
+	
+		<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile"></div>
+		</div>
+		</div>
+		</div>
+		</div>
+		<!-- The Team --><!-- //The Team -->
+		</div>
+		</li>
+	';
+
+	?>
+	<li class="sky-tab-content-4" style="width: 80%;margin-left: 10%;"></li>
+</ul>
+</div>
+<!--/ tabs --><!-- FOOTER -->
+
+<footer>
+<div class="container">
+<div class="col-md-3">
+<h4 class="space30">Quick Links</h4>
+
+<ul class="f-posts">
+	<li><a href="about.html">About Us</a></li>
+	<li><a href="services.html">Services</a></li>
+	<li><a href="news.html">News</a></li>
+	<li><a href="careers.html">Career</a></li>
+	<li><a href="contact.html">Contact Us</a></li>
+</ul>
+</div>
+
+<div class="col-md-5">
+<h4 class="space30">Contact</h4>
+
+<div class="f-social"></div>
+
+<ul class="c-info">
+	<li style="text-align:justify;">P.A.R.I Technologies (M) Sdn Bhd (1232138-M)<br />
+	03-12-11, Tower 3, UOA Business Park <br />
+	Jalan Pengaturcara U1/51A<br />Kawasan Perindustrian Temasya</br>
+	40150 Shah Alam<br />
+	Selangor D.E., Malaysia</li>
+	<li></li>
+	<li>+60 3-5567 9515</li>
+	<li>+60 12-209 1895</li>
+	<li>info@pari.com.my</li>
+</ul>
+</div>
+
+<div class="row">
+<div class="col-md-4"><iframe allowfullscreen="" frameborder="0" height="250" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3984.034263808653!2d101.57992991475712!3d3.0855302977531083!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc4d3d0bc5a403%3A0xb6e013a2ec3a8e51!2sP.A.R.I.+Technologies+(M)+Sdn+Bhd!5e0!3m2!1sen!2smy!4v1535077127686" style="border:0;   box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);" width="600"></iframe></div>
+</div>
+</div>
+</footer>
+
+<!-- FOOTER COPYRIGHT -->
+
+<div class="footer-bottom">
+<div class="container">
+<div class="row">
+<div class="col-md-7">
+<p>&copy; Copyright 2017. P.A.R.I Technologies.</p>
+</div>
+</div>
+</div>
+</div>
+<!-- /.content-wrapper --></div>
+<!-- .offcanvas-pusher --></div>
+<!-- /#main-wrapper --><!-- Preloader -->
+
+<div id="preloader">
+<div id="status">
+<div class="status-mes"></div>
+</div>
+</div>
+<!-- jQuery --><script src="js/jquery.js"></script><!-- Plugins --><script src="js/bootstrap.min.js"></script><script src="js/menu.js"></script><script src="js/owl-carousel/owl.carousel.min.js"></script><script src="js/rs-plugin/js/jquery.themepunch.tools.min.js"></script><script src="js/rs-plugin/js/jquery.themepunch.revolution.min.js"></script><script src="js/jquery.easing.min.js"></script><script src="js/isotope/isotope.pkgd.js"></script><script src="js/jflickrfeed.min.js"></script><script src="js/tweecool.js"></script><script src="js/flexslider/jquery.flexslider.js"></script><script src="js/easypie/jquery.easypiechart.min.js"></script><script src="js/jquery-ui.js"></script><script src="js/jquery.appear.js"></script><script src="js/jquery.inview.js"></script><script src="js/jquery.sticky.js"></script><script src="js/magnific-popup/jquery.magnific-popup.min.js"></script><script src="js/main.js"></script><script>$(function(){
+         $('.carousel').carousel()
+          $('a[title]').tooltip();
+          });
+          function onHover()
+          {
+          $("#menuImg").attr('src', 'images/team/02.jpg');
+          }
+
+          function offHover()
+          {
+          $("#menuImg").attr('src', 'images/team/01.jpg');
+          }
+
+      </script></div>
+</div>
+</body>
+</html>
--- sendmail.php
+++ sendmail.php
@@ -1,6 +1,7 @@
 <?php
 //require 'vendor/autoload.php'; // If you're using Composer (recommended)
 // Comment out the above line if not using Composer
+if(isset($_POST)) {
 require("sendgrid-php/sendgrid-php.php");
 
 // If not using Composer, uncomment the above line and
@@ -8,21 +9,38 @@ require("sendgrid-php/sendgrid-php.php");
 // replacing <PATH TO> with the path to the sendgrid-php.php file,
 // which is included in the download:
 // https://github.com/sendgrid/sendgrid-php/releases
+$name = $_POST['name'];
+$email1 = $_POST['email'];
+$message = $_POST['message'];
+
 $email = new \SendGrid\Mail\Mail(); 
-$email->setFrom("sampratid@gmail.com", "Example User");
-$email->setSubject("Sending with SendGrid is Fun");
-$email->addTo("sampratid@gmail.com", "Example User");
-$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+$email->setFrom("sampratid@gmail.com", "Samprati Dash");
+$email->setSubject("You have recieveed email from website");
+$email->addTo("sampratid@gmail.com", "samprati dash");
+
 $email->addContent(
-    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+    "text/html", "  <tr>
+    <td align='center' valign='top' style='border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%; font-size: 24px; font-weight: bold; line-height: 130%;
+        padding-top: 25px;
+        color: #D02D69;
+        font-family: 'Montserrat', sans-serif;' class='header'>
+            Welcome you have a new message from:</br> $name </br> , $email1,</br> $message .</br>
+    </td>
+</tr>
+
+"
 );
 $sendgrid = new \SendGrid('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');
 // $sendgrid = new \SendGrid(getenv('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'));
 try {
     $response = $sendgrid->send($email);
-    print $response->statusCode() . "\n";
-    print_r($response->headers());
-    print $response->body() . "\n";
+    // print $response->statusCode() . "\n";
+    // print_r($response->headers());
+    // print $response->body() . "\n";
+    header("Location: index.php"); 
+    exit();
+
 } catch (Exception $e) {
     echo 'Caught exception: '. $e->getMessage() ."\n";
+}
 }
